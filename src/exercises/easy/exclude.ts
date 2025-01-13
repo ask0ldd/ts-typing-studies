960ebcb362fc2ef94161ea3931d644507d0c5e77
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-01-13 16:47:24 +0100

    *add* exercise : exclude type

---
+++ src/exercises/easy/exclude.ts
@@ -0,0 +1,48 @@
+/*
+  43 - Exclude
+  -------
+  by Zheeeng (@zheeeng) #easy #built-in #union
+
+  ### Question
+
+  Implement the built-in `Exclude<T, U>`
+
+  > Exclude from `T` those types that are assignable to `U`
+
+  For example:
+
+  ```ts
+  type Result = MyExclude<'a' | 'b' | 'c', 'a'> // 'b' | 'c'
+  ```
+
+  > View on GitHub: https://tsch.js.org/43
+*/
+
+/* _____________ Your Code Here _____________ */
+
+type MyExclude<T, U> = T extends U ? never : T; // extends used as a comparison operator in some way
+
+//
+
+const obj : 'a' | 'b' | 'c' = "a"
+
+const obj2 : MyExclude<'a' | 'b' | 'c', 'c' | 'b'> = "a"
+// const obj3 : MyExclude<'a' | 'b' | 'c', 'c' | 'b'> = "b"
+
+// exclude all the types of the left union that are part of the right union
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type cases = [
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a'>, 'b' | 'c'>>,
+  Expect<Equal<MyExclude<'a' | 'b' | 'c', 'a' | 'b'>, 'c'>>,
+  Expect<Equal<MyExclude<string | number | (() => void), Function>, string | number>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/43/answer
+  > View solutions: https://tsch.js.org/43/solutions
+  > More Challenges: https://tsch.js.org
+*/
